a372ecb91bc0fabc45fa69c94f37b7d7ceba4290
Angelica Miranda <mirandas.angelica@gmail.com>
2020-05-27 15:33:57 -0500

    scrolling direction function

--- script.js
+++ script.js
@@ -18,39 +18,47 @@
   * method to get item being called
   * method to get if screen is scrolling direction
   */
-var observation = function () {
-  this.hello = function() {
-    return 'hello!';
-  }
 
-  this.goodbye = function() {
-    return 'goodbye!';
+
+const setOptions = ({
+  root = null,
+  rootMargin = '0px',
+  threshold = 1
+} = {}) => ({
+  root,
+  rootMargin,
+  threshold
+});
+
+/**
+ * This function initializes a scrolling direction tracker
+ */
+let scrollingDirection = () => {
+  let currentY = window.scrollY;
+  let previousY = 0;
+
+  let isDownwards = () => {
+    currentY = window.scrollY;
+    return currentY > previousY;
+  }
+  let updatePrevious = () => previousY = currentY;
+
+  let getDirection = () => {
+    const down = isDownwards();
+    updatePrevious();
+    return down;
   }
   return {
-    hello: this.hello,
-    goodbye: this.goodbye,
+    isScrollingDown: getDirection
   }
-};
-observation().hello;
-
-
-
-let previousY = 0;
-let currentY = window.scrollY;
-let scrollingDown = currentY > previousY;
-let options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 1
 }
 
+let track = scrollingDirection();
+
 let transitionEffect = (entries) => { 
+  //console.log('entries', entries);
+  console.log(track.isScrollingDown());
   entries.forEach((entry, index) => {
-    if (currentY != window.scrollY) {
-      previousY = currentY;          
-      currentY = window.scrollY;
-      scrollingDown = currentY > previousY;
-    }
     if (entry.isIntersecting) {
       //console.log(entry,'bhsjhbjsd', index);
       //console.log('scrollingDown', scrollingDown)
@@ -59,8 +67,11 @@ let transitionEffect = (entries) => {
   });
 };
 
+let selectItems = (selector) => document.querySelectorAll(selector);
+
+//console.log(selectItems('.item'), 'hbsja');
 let targets = document.querySelectorAll('.item');
-let observer = new IntersectionObserver(transitionEffect, options);
+let observer = new IntersectionObserver(transitionEffect, setOptions());
 targets.forEach((t, i) => {
   //console.log(t,i,'hbdbhjdsbhj');
   observer.observe(t);
