eaa981c6d0efbc2cfce3549abb772f1d5447f73e
Angelica Miranda <mirandas.angelica@gmail.com>
2020-06-02 19:25:46 -0500

    add links and animation on documentation

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-# Options
+# Intersecta
+Add an animation effect on a DOM element when scrolling using only JavaScript.
+
+No external libs only WebAPI. It is supported on most browsers.
+However, for more detailed browser compatibility check:
+
+[Intersection Observer](https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver#Browser_compatibility)
+
+[Element Animation](https://developer.mozilla.org/en-US/docs/Web/API/Element/animate#Browser_compatibility)
+## Getting started
+Just start **intersecta** initializing it with your options object. It only requires the CSS selector for the element you want to animate on scroll.
+
+## Options
 | Name        | Type        | Default     | Description  |
 | :---        |    :----:   |    :----:   | :---         |
 | selector    | string      | null        | *Required*. CSS selector for the observed element. |
 | threshold   | number      | 1           | A number between 0.0 and 1.0, specifying a ratio of intersection area to total bounding box area to trigger the event.
-| animation   | string      | "fadeIn"    | Name of the animation.
+| animation   | string      | "fadeIn"    | Name of the animation, choose one among the options.
 | duration    | number      | 1000        | Number of milliseconds the animation takes to complete.
 | delay       | number      | 0           | Number of milliseconds to delay the start of the animation.
 | easing      | string      | "linear"      | Rate of the animation's change over time.
 | once        | boolean     | true          | Allows animation to be reapeated everytime the element enters.
+
+## Animations
+* fadeIn
+* fadeOut
--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 /**
  * Options for the intersection observer API
  * 
- * More information at: https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver
- * 
  * @param {Element} root object whose bounding box is used as the bounds of the viewport 
  * @param {string} rootMargin offsets for one or more sides of the root's bounding box.
  * @param {number} threshold list or value of intersection thresholds for the observer
@@ -18,7 +16,7 @@ setObserverOptions = ({
   threshold
 });
 
-setanimationOptions = ({
+setAnimationOptions = ({
   selector = null,
   threshold = 1,
   animation = "fadeIn",
@@ -67,14 +65,13 @@ let withScrollingDirection = () => {
 handleEntry = (entry, observer, options) => {
   let { duration, fill, delay, easing } = options;
   let animationOptions = { duration, fill, delay, easing };
-  console.log(options.animation, animationKeyFrames[options.animation]);
   if (entry.isIntersecting) {
     entry.target.animate(
       // keyframes
       animationKeyFrames[options.animation],
       // timing options
       animationOptions
-    )
+    );
     // when enabled, remove observer after effect played once
     if (options.once) observer.unobserve(entry.target);
   } else {    
@@ -87,19 +84,20 @@ handleEntry = (entry, observer, options) => {
   }
 }
 
-let createObserver = (options) => {
-  if (!options.selector) throw new Error ("No selector was given, expected a string");
+let createObserver = (userOptions) => {
+  if (!userOptions.selector) throw new Error ("No selector was given, expected a string");
   
+  let options = setAnimationOptions(userOptions);
   let selectItems = document.querySelectorAll(options.selector);
   let intersectionEffect = (entries, observer) => entries.forEach(entry => handleEntry(entry, observer, options));
   let observer = new IntersectionObserver(intersectionEffect, setObserverOptions(options));
   selectItems.forEach(t => observer.observe(t));
 } 
-let obs = createObserver(setanimationOptions({
+let obs = createObserver({
   selector: ".item",
   once: false,
-  animation: "fadeOut",
-}));
+  animation: "fadeIn",
+});
 
 let animationKeyFrames = {
   fadeIn: [
