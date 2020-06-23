0859da7a466897ca53e31f4c6ef2d69a5b611fd0
Angelica Miranda <mirandas.angelica@gmail.com>
2020-06-23 13:40:22 -0500

    update git ignore

---
+++ src/intersecta/frames.js
@@ -0,0 +1,47 @@
+const frames = {
+  fadeIn: [
+    { opacity: 0 },
+    { opacity: 1 },
+  ],
+  fadeOut: [
+    { opacity: 1 },
+    { opacity: 0 },
+  ],
+  zoomIn: [
+    { transform: 'scale(1, 1)' },
+    { transform: 'scale(1.3, 1.3)' },
+    { transform: 'scale(1, 1)' },
+  ],
+  zoomOut: [
+    { transform: 'scale(1, 1)' },
+    { transform: 'scale(0.7, 0.7)' },
+    { transform: 'scale(1, 1)' },
+  ],
+  slideDown: [
+    { transform: 'translateY(-50%)', opacity: 0 },
+    { transform: 'translateY(0)', opacity: 1 },
+  ],
+  slideUp: [
+    { transform: 'translateY(50%)', opacity: 0 },
+    { transform: 'translateY(0)', opacity: 1 },
+  ],
+  slideLeft: [
+    { transform: 'translateX(50%)', opacity: 0 },
+    { transform: 'translateX(0)', opacity: 1 },
+  ],
+  slideRight: [
+    { transform: 'translateX(-50%)', opacity: 0 },
+    { transform: 'translateX(0)', opacity: 1 },
+  ],
+  flipLeft: [
+    { transform: 'rotateY(-360deg)' },
+    { transform: 'rotateY(0)' },
+  ],
+  flipRight: [
+    { transform: 'rotateY(360deg)' },
+    { transform: 'rotateY(0)' },
+  ],
+};
+
+const onceOnlyFrames = new Set(['slideDown', 'slideUp', 'zoomIn']);
+export { frames, onceOnlyFrames };
