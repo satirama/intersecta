0a34e063a97093b5d82d7fdd3ff50e7cd5b27899
Angelica Miranda <mirandas.angelica@gmail.com>
2020-10-30 20:00:16 -0600

    deploy

---
+++ precache-manifest.7144163097074e031adf9925c945bbf0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7510a86bb5c8fc383d",
+    "url": "/intersecta/css/app.621aa31d.css"
+  },
+  {
+    "revision": "2808af0ceeecefe099dc",
+    "url": "/intersecta/css/chunk-vendors.a2cadc9e.css"
+  },
+  {
+    "revision": "82079f23cc8f6e5f0ef19c33c055baf6",
+    "url": "/intersecta/img/circle.png"
+  },
+  {
+    "revision": "ecc4cf252a967f043b7cc229560486ac",
+    "url": "/intersecta/img/github.png"
+  },
+  {
+    "revision": "11d38b71ab182e64a9440177f00030d4",
+    "url": "/intersecta/img/gitlab.png"
+  },
+  {
+    "revision": "c94ad85495208197e60f12bf0078f5c4",
+    "url": "/intersecta/img/scroll.png"
+  },
+  {
+    "revision": "185b34606a228d2843220b2612aaac28",
+    "url": "/intersecta/img/twitter.png"
+  },
+  {
+    "revision": "546018ea6c518572aed5a3a43d63e6f5",
+    "url": "/intersecta/index.html"
+  },
+  {
+    "revision": "ed7510a86bb5c8fc383d",
+    "url": "/intersecta/js/app.22ea91c5.js"
+  },
+  {
+    "revision": "2808af0ceeecefe099dc",
+    "url": "/intersecta/js/chunk-vendors.9c87442a.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/intersecta/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/intersecta/robots.txt"
+  }
+]);
